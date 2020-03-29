8a2a8e7bbfd80cf67d2f86d75f15813448d14445
Matej Snuderl <matej.snuderl@cognite.com>
2020-03-29 19:53:31 +0200

    styling: expanding input width

--- src/modules/exchange/components/CurrencyView/NumberInput.tsx
+++ src/modules/exchange/components/CurrencyView/NumberInput.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Input, InputProps } from 'baseui/input';
 import { useStyletron } from 'baseui';
+import merge from 'lodash/merge';
 
 type Props = InputProps;
 
 const NUMBER_INPUT_PATTERN = /^(\d+\.?\d{0,2})?$/;
 
-const NumberInput = ({ onChange, ...rest }: Props) => {
+const NumberInput = ({ onChange, overrides, ...rest }: Props) => {
   const [_, theme] = useStyletron();
 
   const onNumberInputChange = (event: React.FormEvent<HTMLInputElement>) => {
@@ -22,28 +23,31 @@ const NumberInput = ({ onChange, ...rest }: Props) => {
       placeholder="0"
       onChange={onNumberInputChange}
       size="compact"
-      overrides={{
-        Root: { style: { width: 'auto', justifyContent: 'flex-end' } },
-        Input: {
-          style: {
-            'text-align': 'right',
-            caretColor: theme.colors.accent400,
-            paddingLeft: '0px',
-            paddingRight: '0px',
-            paddingTop: '0px',
-            paddingBottom: '0px',
+      overrides={merge(
+        {
+          Root: { style: { width: 'auto', justifyContent: 'flex-end' } },
+          Input: {
+            style: {
+              'text-align': 'right',
+              caretColor: theme.colors.accent400,
+              paddingLeft: '0px',
+              paddingRight: '0px',
+              paddingTop: '0px',
+              paddingBottom: '0px',
+            },
+          },
+          InputContainer: {
+            style: {
+              borderWidth: '0px',
+              backgroundColor: 'transparent',
+            },
+          },
+          StartEnhancer: {
+            style: { backgroundColor: 'transparent', color: theme.colors.black },
           },
         },
-        InputContainer: {
-          style: {
-            borderWidth: '0px',
-            backgroundColor: 'transparent',
-          },
-        },
-        StartEnhancer: {
-          style: { backgroundColor: 'transparent', color: theme.colors.black },
-        },
-      }}
+        overrides
+      )}
       {...rest}
     />
   );
